67b41e3bd37e0c1b4d72b065bc353f9d83b51455
ChoiJaeYeong <chlwodud2@naver.com>
2021-10-31 23:23:37 +0900

    Git Hook 추가 task 생성

--- app/src/main/java/com/tambi/cicdtest/MainActivity.kt
+++ app/src/main/java/com/tambi/cicdtest/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.tambi.cicdtest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun hi() {
+        Log.d("this", "hi")
     }
 }
